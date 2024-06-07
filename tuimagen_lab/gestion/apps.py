2e0ba469f818aa22a6e377fa4fa22947a062e665
Daniher01 <daniher02@gmail.com>
2024-06-07 17:13:56 -0400

    se crea logica para cargar los datos por defecto de pieza y materiales

--- tuimagen_lab/gestion/apps.py
+++ tuimagen_lab/gestion/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
-
 class GestionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'gestion'
+
+    def ready(self):
+        from . import initial_data
+        from django.db.models.signals import post_migrate
+        post_migrate.connect(initial_data.create_initial_piezas, sender=self)
+        post_migrate.connect(initial_data.create_initial_materiales, sender=self)
