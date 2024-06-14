9b9248df4af4d6ec6f891a95842fe37eb09b915d
Daniher01 <daniher02@gmail.com>
2024-06-14 13:48:58 -0400

    se valida el rut

--- app/tuimagen_lab/static/js/validaciones/validar_formularios.js
+++ app/tuimagen_lab/static/js/validaciones/validar_formularios.js
@@ -1,10 +1,16 @@
 function validarFormulario(form) {
-
     // Validar que los campos del formulario no estén vacíos
     if (!form.checkValidity()) {
         form.classList.add('was-validated');
+    } else {
+        form.classList.remove('was-validated');
+    }
+
+    // Verificar si hay algún elemento con la clase 'is-invalid'
+    const invalidElements = form.querySelectorAll('.is-invalid');
+    if (invalidElements.length > 0) {
         return false;
     }
-    form.classList.remove('was-validated');
-    return true;
+
+    return form.checkValidity();
 }
