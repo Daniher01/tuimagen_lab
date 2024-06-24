713606bd29d617fb8aa6df1f2382571b3e7adb00
Daniher01 <daniher02@gmail.com>
2024-06-24 13:08:21 -0400

    se logra terminar un trabajo

--- app/tuimagen_lab/trabajos/urls.py
+++ app/tuimagen_lab/trabajos/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import seleccionar_tipo_trabajo, ver_trabajos_pendientes
+from .views import seleccionar_tipo_trabajo, ver_trabajos_pendientes, terminar_trabajo
 from fresado import views as fresado
 from impresion3d import views as impresion3d
 from escaneos_intraorales import views as escaner_intraoral
@@ -15,4 +15,6 @@ urlpatterns = [
     path('detalle/fresado/<int:trabajo_id>', fresado.detalle_trabajo_fresado, name='detalle_trabajo_fresado'),
     path('detalle/impresion3d/<int:trabajo_id>', impresion3d.detalle_trabajo_impresion3d, name='detalle_trabajo_impresion3d'),
     path('detalle/escaneo_intraoral/<int:trabajo_id>/', escaner_intraoral.detalle_trabajo_escaneo_intraoral, name='detalle_trabajo_escaneo_intraoral'),
+    # URLs para terminar un trabajo
+    path('terminar_trabajo/', terminar_trabajo, name='terminar_trabajo'),
 ]
--- app/tuimagen_lab/trabajos/views.py
+++ app/tuimagen_lab/trabajos/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Count
 from django.http import JsonResponse
+from django.utils import timezone
 from .forms import TrabajoForm
 from fresado.forms import PiezaForm
 from pacientes.forms import PacienteForm
@@ -32,3 +35,18 @@ def ver_trabajos_pendientes(request):
         item['tipo_trabajo'] = TIPOS_DICT.get(item['tipo_trabajo'], item['tipo_trabajo'])
   
     return render(request, 'trabajos/listar_trabajos.html', {'trabajos': trabajos, 'total_trabajos': total_trabajos, 'estado': ESTADO})
+
+@login_required
+@require_POST
+@csrf_exempt
+def terminar_trabajo(request):
+    ESTADO = 'terminado'
+    trabajo_id = request.POST.get('id')
+    trabajo = get_object_or_404(Trabajo, id=trabajo_id)
+    
+    if trabajo.estado != ESTADO:
+        trabajo.estado = ESTADO
+        trabajo.fecha_termino = timezone.now()
+        trabajo.save()
+        return JsonResponse({'success': True})
+    return JsonResponse({'success': False, 'error': 'El trabajo ya está terminado'})
