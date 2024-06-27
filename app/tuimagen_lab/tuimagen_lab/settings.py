2ecca4aebeb227e6d07aa04d0f42b120c6493b33
Daniher01 <daniher02@gmail.com>
2024-06-27 18:02:04 -0400

    feat(build): se actualiza el dockercmpose de produccion

--- app/tuimagen_lab/tuimagen_lab/settings.py
+++ app/tuimagen_lab/tuimagen_lab/settings.py
@@ -90,9 +90,17 @@ WSGI_APPLICATION = 'tuimagen_lab.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DATABASE_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('DATABASE_NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.getenv('DATABASE_USER', ''),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', ''),
+        'HOST': os.getenv('DATABASE_HOST', 'localhost'),
+        'PORT': os.getenv('DATABASE_PORT', '3306'),
     }
 }
 
